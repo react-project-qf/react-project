187146623470626cb473520d40b358b3bbad546b
nisisong <1017735045@qq.com>
2017-03-28 19:35:34 +0800

    a

--- src/script/app.js
+++ src/script/app.js
@@ -1,29 +1,6 @@
 require('../style/app.scss')
 
 import React from 'react'
-<<<<<<< HEAD
-import ReactDOM from 'react-dom'
-import { Router, Route, IndexRoute, hashHistory } from 'react-router'
-
-import Index from './component/index'
-import Board from './component/board'
-import Search from './component/search'
-import Cart from './component/cart'
-import My from './component/my'
-
-ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Index}>
-      <IndexRoute component={Board}></IndexRoute>
-      <Route path="board" component={Board}></Route>
-      <Route path="search" component={Search}></Route>
-      <Route path="cart" component={Cart}></Route>
-      <Route path="my" component={My}></Route>
-    </Route>
-  </Router>,
-  document.getElementById('root')
-)
-=======
 import ReactDom from 'react-dom'
 import {
 	Router,
@@ -61,4 +38,3 @@ ReactDom.render(
 	</Provider>,
 	document.getElementById('root')
 );
->>>>>>> master
