d4b5e5cf6149ec219682d81bc37a0d2440c40736
nisisong <1017735045@qq.com>
2017-03-28 19:38:45 +0800

    a

--- src/script/component/my.js
+++ src/script/component/my.js
@@ -1,30 +1,14 @@
 import React from 'react'
 
-<<<<<<< HEAD
-class My extends React.Component {
-  render() {
-    return (
-      <div className="m-my">
-        <div className="guang">
-        	<img src="//img1.qunarzz.com/qs/1610/a6/01d1ad00e4b9e102.jpg" />
-        </div>
-        <ul>
-			<li><p>空空如也，去装满它</p></li>
-			<li><span>随意逛逛~</span></li>
-        </ul>
-=======
+
 class My extends React.Component{
   render(){
     return(
       <div>
         <h2>我的</h2>
->>>>>>> master
       </div>
     )
   }
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> master
 export default My
