4d076e5023322aaa95b32ed8a18fd5679ad3a7de
nisisong <1017735045@qq.com>
2017-04-01 09:34:20 +0800

    订单更新

--- src/script/component/now.js
+++ src/script/component/now.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
 class Now extends React.Component {
-	
+	constructor(props) {
+      super(props);
+      this.state = {}
+    }
   render() {
     return (
-      <div className="m-now">
-       纳尼纳尼女
-      </div>
+    	<div className="now">
+	       <div className="cartNone">
+			  <img src="./images/orders_empty_icon.png" alt=""/>
+			  <p>你还没有订单呢，赶紧下单吧</p>
+			  <div className="btn" onClick={()=>{location.href='#/home';}}>去关注</div>
+			</div>
+		</div>
     )
   }
 }
